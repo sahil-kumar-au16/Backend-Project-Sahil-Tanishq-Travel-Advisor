5cc780585d65e9bef066ed2a4649b4c85b1bce1a
sahil-kumar-au16 <70127609+sahil-kumar-au16@users.noreply.github.com>
2021-06-07 00:51:39 +0530

    Delete Downloads/tanishqtravel/lets-travel-master directory

--- Downloads/tanishqtravel/lets-travel-master/Paytm/config.js
+++
@@ -1,7 +0,0 @@
-var PaytmConfig = {
-  mid: "ChLyYB50294673112568",
-  key: "oPhqB59ilqxNIAVd",
-  website: "WEBSTAGING"
-}
-
-module.exports.PaytmConfig = PaytmConfig
--- Downloads/tanishqtravel/lets-travel-master/public/js/posts.js
+++
@@ -1,5 +0,0 @@
-async function getPosts(){
-  return await fetch('http://localhost:3000/posts')
-        .then((res) => res.json())
-        .then((data) => data);
-}
